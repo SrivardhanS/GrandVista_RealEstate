df086c052b8c9c1640070b4f814e794abef907ac
Srivardhan S <srivardhansivakumar@gmail.com>
2024-08-01 23:56:08 +0530

    intitial half ui

---
+++ api/routes/post.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const router = express.Router()
+
+// router.get("/test", (res, req) =>{
+//     console.log("router works !")
+// });
+
+// router.post("/test", (res, req) =>{
+//     console.log("router works !")
+// });
+
+// router.put("/test", (res, req) =>{
+//     console.log("router works !")
+// });
+
+// router.delete("/test", (res, req) =>{
+//     console.log("router works !")
+// });
+
+export default router;
